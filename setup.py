f66a5e94cac97a6a1d14eca33ae146cddf99e8e1
stefanbie <bie.stefan@gmail.com>
2016-11-18 14:54:48 +0100

    new setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='framewalker',
+    version='0.1',
+    url='https://github.com/stefanbie/FrameWalker',
+    install_requires=['peewee', 'PyMySQL']
+    )
