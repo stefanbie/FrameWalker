e711af40e50db08783bfee6b6c2808514459a7a5
stefanbie <bie.stefan@gmail.com>
2016-11-21 16:51:20 +0100

    Changed version nbr

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='framewalker',
-    version='0.1',
+    version='0.2',
     url='https://github.com/stefanbie/FrameWalker',
     install_requires=[
         'peewee',
