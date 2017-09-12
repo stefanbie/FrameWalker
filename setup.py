c3e6de8865cefe8c6a5839b1d4b9991265663064
Stefan Bie <Stefan.Bie@systemverification.com>
2017-09-12 15:15:49 +0200

    new project structure

--- setup.py
+++ setup.py
@@ -2,11 +2,8 @@ from setuptools import setup
 
 setup(
     name='framewalker',
-    version='0.4',
+    version='0.6',
     url='https://github.com/stefanbie/FrameWalker',
-    install_requires=[
-        'peewee',
-        'PyMySQL',
-    ],
-    py_modules=['Timings','IFrame', 'JavaScript','DB']
+    install_requires=['peewee','PyMySQL',],
+    py_modules=['Timings','IFrame', 'JavaScript','DB','Common']
     )
