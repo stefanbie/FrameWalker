cda58c0815b88795819bbc6c4c7683503b6ff869
Stefan Bie (EXSTBIE) <EXSTBIE@ikeares-pubtst.com>
2017-10-26 15:30:18 +0200

    changed from EC.presence_of_element_located to EC.element_to_be_clickable

--- Common.py
+++ Common.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import TimeoutException
 
 driver = None
 
@@ -28,13 +29,18 @@ def newBP():
     return driver
 
 
-def click_by_xpath(xpath, timeout=30):
-    element = WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH, xpath)))
+def click_by_xpath(xpath, timeout=30, throwEx=True):
+    try:
+        element = WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+    except TimeoutException:
+        if throwEx:
+            raise(TimeoutException)
+        return
     element.click()
 
 
 def find_by_xpath(xpath, timeout=30):
-    element = WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH, xpath)))
+    element = WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, xpath)))
     return element
 
 
@@ -47,12 +53,12 @@ def js_click(element):
 
 
 def js_click_by_xpath(xpath, timeout=30):
-    element = WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH, xpath)))
+    element = WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, xpath)))
     driver.execute_script("arguments[0].click();", element)
 
 
 def js_click_by_xpath_async(xpath, timeout=30):
-    element = WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH, xpath)))
+    element = WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, xpath)))
     driver.execute_script("var _e=arguments[0]; setTimeout(function() { _e.click(); }, 100);", element)
 
 
